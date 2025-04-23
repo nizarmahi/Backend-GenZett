f1291c93d2c58d7547d5aaca8de18f49997b41e7
nizarmahi <mahinizar7@gmail.com>
2025-04-23 11:48:39 +0700

    .

---
+++ database/migrations/2025_04_23_035431_create_reservation_member.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservation_member', function (Blueprint $table) {
+            $table->id("reservationMemberId");
+            $table->unsignedBigInteger('reservationId');
+            $table->foreign('reservationId')->references('reservationId')->on('reservations')->onDelete('cascade');
+            $table->unsignedBigInteger('membershipId');
+            $table->foreign('membershipId')->references('membershipId')->on('memberships')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservation_member');
+    }
+
+
+};

165034efd4afd497987894988f5fbc5136e0b9d8
Alhamdanaaa <aalhamdana@gmail.com>
2025-05-09 09:22:50 +0700

    feat: schedule

---
+++ app/Http/Controllers/API/ScheduleController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ScheduleController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = DB::table('reservations')
+            ->join('reservation_details', 'reservations.reservationId', '=', 'reservation_details.reservationId')
+            ->join('fields', 'reservation_details.fieldId', '=', 'fields.fieldId')
+            ->join('times', 'reservation_details.timeId', '=', 'times.timeId')
+            ->join('sports', 'fields.sportId', '=', 'sports.sportId')
+            ->join('locations', 'fields.locationId', '=', 'locations.locationId')
+            ->select([
+                // 'reservations.reservationId',
+                'reservations.name',
+                'reservation_details.date',
+                'times.time as fieldTime',
+                'fields.fieldId',
+                // 'sports.sportName as sport',
+                // 'locations.locationName as location',
+                'reservations.paymentStatus'
+            ]);
+
+        // Filter by sport
+        if ($request->has('sportId')) {
+            $query->where('sports.sportId', $request->sportId);
+        }
+
+        // Filter by location
+        if ($request->has('locationId')) {
+            $query->where('locations.locationId', $request->locationId);
+        }
+
+        $schedules = $query->get();
+
+        return response()->json($schedules);
+    }
+}

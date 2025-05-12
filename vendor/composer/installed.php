4475db02a36f6934e26632f17d940f416ce6a485
nizarmahi <mahinizar7@gmail.com>
2025-05-12 16:45:59 +0700

    feat: update LocationSeeder Malang & install OpenAPI

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'dev-development',
-        'version' => 'dev-development',
-        'reference' => '2a865f9708cc799e7a16e4d7933adcbc73d78e9a',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f8bc488a77a884db2d0a94b2bd1ce5f27f179901',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -397,7 +397,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '0a5a6cd1297466f6df01d7fa6d9e6398d7cdea88',
+            'reference' => 'f8bc488a77a884db2d0a94b2bd1ce5f27f179901',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),

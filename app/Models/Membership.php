d64ac8dc567c8023c93a2d7843239ca109d4c42c
febriantimayori <febriantimayori@gmail.com>
2025-05-21 22:59:30 +0700

    feat(model): update fillable discount in Membership

--- app/Models/Membership.php
+++ app/Models/Membership.php
@@ -11,7 +11,7 @@ class Membership extends Model
 
     protected $primaryKey = 'membershipId';
 
-    protected $fillable = ['locationId','sportId', 'name', 'description', 'price', 'weeks'];
+    protected $fillable = ['locationId','sportId', 'name', 'description', 'discount', 'weeks'];
     protected $casts = [
         'created_at' => 'datetime',
     ];

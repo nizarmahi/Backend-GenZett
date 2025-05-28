d9ba9747f34ad6d151748fe9795bb6d2ed351412
Alhamdanaaa <aalhamdana@gmail.com>
2025-05-28 09:45:18 +0700

    fix: edit status to paymentStatus

--- database/seeders/ReservationSeeder.php
+++ database/seeders/ReservationSeeder.php
@@ -14,7 +14,7 @@ class ReservationSeeder extends Seeder
             ['userId' => 6, 'name' => 'Booking 1', 'paymentStatus' => 'pending', 'total' => 150000],
             ['userId' => 7, 'name' => 'Booking 2', 'paymentStatus' => 'dp', 'total' => 200000],
             ['userId' => 8, 'name' => 'Booking 3', 'paymentStatus' => 'complete', 'total' => 250000],
-            ['userId' => 9, 'name' => 'Booking 4', 'paymentStatus' => 'pending', 'total' => 180000],
+            ['userId' => 9, 'name' => 'Booking 4', 'paymentStatus' => 'dp', 'total' => 180000],
             ['userId' => 10, 'name' => 'Booking 5', 'paymentStatus' => 'complete', 'total' => 300000],
         ];
 

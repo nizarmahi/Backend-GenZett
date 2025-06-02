fff15ab5167e2482e951d6a8940ca9f330425cc6
DivaAji <diva7aji7@gmail.com>
2025-06-02 15:32:11 +0700

    Fix cors.php for local development

--- config/cors.php
+++ config/cors.php
@@ -12,10 +12,14 @@ return [
 
     'allowed_methods' => ['*'], // Izinkan semua metode HTTP
 
+    // 'allowed_origins' => [
+    //     env('FRONTEND_URL', 'http://20.255.60.15:3000'), // URL Frontend Anda
+    //     // 'http://localhost:3000', // Untuk pengembangan lokal jika perlu
+    // ],
     'allowed_origins' => [
-        env('FRONTEND_URL', 'http://20.255.60.15:3000'), // URL Frontend Anda
-        // 'http://localhost:3000', // Untuk pengembangan lokal jika perlu
-    ],
+    env('FRONTEND_URL', 'http://20.255.60.15:3000'), // URL Frontend Anda
+    'http://localhost:3000', // Untuk pengembangan lokal
+],
 
     'allowed_origins_patterns' => [], // Gunakan ini jika origin Anda dinamis dengan pola regex
 

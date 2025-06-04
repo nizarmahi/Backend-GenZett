a1e5a458545534708276a8061f63c0ca992104c8
DivaAji <diva7aji7@gmail.com>
2025-06-04 19:44:14 +0700

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -8,22 +8,22 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'], // Sesuaikan dengan path API Anda
+    'paths' => ['api/*', 'sanctum/csrf-cookie'], 
 
-    'allowed_methods' => ['*'], // Izinkan semua metode HTTP
+    'allowed_methods' => ['*'], 
 
     // 'allowed_origins' => [
-    //     env('FRONTEND_URL', 'http://20.255.60.15:3000'), // URL Frontend Anda
-    //     // 'http://localhost:3000', // Untuk pengembangan lokal jika perlu
+    //     env('FRONTEND_URL', 'http://20.255.60.15:3000'), // URL Frontend 
+    //     // 'http://localhost:3000', 
     // ],
     'allowed_origins' => [
-    env('FRONTEND_URL', 'http://20.255.60.15:3000'), // URL Frontend Anda
-    'http://localhost:3000', // Untuk pengembangan lokal
+    env('FRONTEND_URL', 'http://20.255.60.15'), // URL Frontend 
+    'http://localhost:3000', 
 ],
 
-    'allowed_origins_patterns' => [], // Gunakan ini jika origin Anda dinamis dengan pola regex
+    'allowed_origins_patterns' => [], 
 
-    'allowed_headers' => ['*'], // Izinkan semua header
+    'allowed_headers' => ['*'], 
 
     'exposed_headers' => [],
 
